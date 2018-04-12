7d3a9622162d64ed628fc16a3216605d3d546199
danielepune <38095928+danielepune@users.noreply.github.com>
2018-04-12 09:28:30 +0300

    Add files via upload
    Question 3

--- main.c
+++ main.c
@@ -1,25 +1,39 @@
 
-/*this is a header file do_it with returning a float
-with characters as list of parameters*/
-float do_it(char1,char2,char3)
+// declarration for an array named data
 
-/*this function return any value*/
-void print_a_number (int x)
+long data[50];
 
+// this assigning a float value to an array
 
-#include<stdio.h>
-// alway terminate when u call a function
-void print_msg (void);
-main()
+float data[49]=123.456;
+
+/*when a for loop is run then x values will range from 0 to 99*/
+int x
+for(x=0;x<100;x++)
+    printf("%d\n",x);
+
+//for is aloop hence we expect a range of values
+int ctr;
+for( ctr=2;ctr<10;ctr+=3)
+printf("%d\n", ctr);
+the values printed here will be 2,5,8
+
+// symantics of a while loop
+
+#include <stdio.h>
+
+int main()
 {
-    //the braces should be empty hence should not take any aguments
-print_msg();
-return 0;
-}
-    //the header function should be identical at any point when called
-void print_msg(void)
-{
-    puts("this is the message to print");
-
+    int count=1;
+    while(count<=100)
+        {
+        printf("%d\n",count);
+        count+=3;
+        }
     return 0;
 }
+
+ the for (statement) should not be terminated
+ i.e for(counter=1;counter< MAXVALUES;counter++)
+        printf("\n counter=%d", counter);
+
